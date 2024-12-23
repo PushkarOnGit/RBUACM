34aa55df1849b24d310e96d63579e66c3405b1ca
Mandar <mandargurjar51@gmail.com>
2024-12-23 23:40:08 +0530

    tailwind testing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import styles from "../styles/home.module.css";
 const Home = () => {
   return (
     <div className={styles.container}>
+      <div className="bg-slate-300 text-black p-4 rounded-2xl text-lg font-bold">Hello World</div>
       <h1 className={styles.title}>Welcome to ACM Website</h1>
       <p className={styles.subtitle}>
         Explore our events, team, and testimonials with a modern Metallic Gray theme.
